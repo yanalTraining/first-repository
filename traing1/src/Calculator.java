ab8673f168a23010fb755f5adab9e0a7743814c3
yanal1997 <40598669+yanal1997@users.noreply.github.com>
2018-06-28 08:47:01 +0300

    Update Calculator.java

--- traing1/src/Calculator.java
+++ traing1/src/Calculator.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+// hi I am yanal
 public class Calculator extends Application {
 	/*
 	 * this is simple calculator class
